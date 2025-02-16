1fa410455559b2baecf55cc9829a33fade5836eb
Paulo Geliandes <geliandes@hotmail.com>
2025-02-16 16:18:35 -0300

    login with google

--- src/components/Auth/SocialButtonContainer/index.tsx
+++ src/components/Auth/SocialButtonContainer/index.tsx
@@ -2,11 +2,24 @@ import React from 'react';
 import googleIcon from '@/assets/images/google-icon.png';
 import facebookIcon from '@/assets/images/facebook-icon.png';
 import { SocialButton } from '../SocialButton';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import * as S from './styles';
+import { signIn } from '@/configs/signin';
+import Constants from 'expo-constants';
+
+const webClientId = Constants.expoConfig?.extra?.WEB_CLIENT_ID;
+
+GoogleSignin.configure({
+  webClientId,
+  scopes: ['profile', 'email'],
+  offlineAccess: true,
+  forceCodeForRefreshToken: true,
+});
 
 export function SocialButtonsContainer() {
   const handleGoogleLogin = () => {
-    console.log('Login com Google');
+    console.log('Login com Google 2');
+    signIn();
   };
 
   const handleFacebookLogin = () => {
