4d92d45c87984bf62ff2014a8a5c693635dbdf9b
Paulo Geliandes <geliandes@hotmail.com>
2025-03-02 23:16:20 -0300

    correcoes de tipagem

--- src/components/common/Icons/ValidIcon.tsx
+++ src/components/common/Icons/ValidIcon.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-const SVGComponent = (props) => (
+const SVGComponent = (props: React.SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 16 16"
