b46cc33f5a55e985aba22ca8748bb79a4d5c127b
Paulo Geliandes <geliandes@hotmail.com>
2025-03-23 21:38:46 -0300

    retornando a api interna de login

---
+++ src/pages/api/login/verificar-email.ts
@@ -0,0 +1,20 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import axios, { AxiosError } from 'axios'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const response = await axios.post(
+      `${process.env.BACKEND_URL}/verificar-email`,
+      req.body
+    )
+    return res.status(response.status).json(response.data)
+  } catch (error: unknown) {
+    const axiosError = error as AxiosError
+    return res
+      .status(axiosError.response?.status || 500)
+      .json(axiosError.response?.data || { error: 'Erro interno no servidor' })
+  }
+}
