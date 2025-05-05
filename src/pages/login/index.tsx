a346b0e23c40d9f213467d8cd9a81806eabc00c8
Paulo Geliandes <geliandes@hotmail.com>
2025-05-04 22:56:44 -0300

    title para login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,4 +1,5 @@
 import dynamic from 'next/dynamic'
+import Head from 'next/head'
 
 import * as S from '@/styles/pages/login'
 
@@ -9,6 +10,13 @@ const LoginForm = dynamic(() => import('@/components/forms/LoginForm'), {
 export default function Login() {
   return (
     <S.Wrapper>
+      <Head>
+        <title>Entrar ou Cadastrar – Excursionistas</title>
+        <meta
+          name="description"
+          content="Entre na sua conta ou cadastre-se no Excursionistas para buscar, reservar e gerenciar vagas em caravanas para shows, feiras e eventos com segurança e praticidade."
+        />
+      </Head>
       <LoginForm />
     </S.Wrapper>
   )
