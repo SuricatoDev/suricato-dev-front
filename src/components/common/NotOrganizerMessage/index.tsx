786cfd1892d537ae585145dda7a62ed640311a5f
Paulo Geliandes <geliandes@hotmail.com>
2025-06-16 21:32:48 -0300

    fix: ajuste de build

--- src/components/common/NotOrganizerMessage/index.tsx
+++ src/components/common/NotOrganizerMessage/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import EspereImg from '@/assets/images/espere.png'
-import { motion } from 'framer-motion'
+import { easeIn, easeOut, motion } from 'framer-motion'
 
 import { useIsOrganizer } from '@/hooks/useIsOrganizer'
 
@@ -27,39 +27,34 @@ export default function NotOrganizerMessage({
     visible: {
       opacity: 1,
       y: 0,
-      transition: { duration: 0.6, ease: [0.42, 0, 1, 1] }
+      transition: { duration: 0.6, ease: easeOut }
     },
-    exit: {
-      opacity: 0,
-      y: -20,
-      transition: { duration: 0.3, ease: [0.42, 0, 1, 1] }
-    }
+    exit: { opacity: 0, y: -20, transition: { duration: 0.3, ease: easeIn } }
   }
 
   return (
-    <motion.div
+    <S.Wrapper
+      as={motion.div}
       initial="hidden"
       animate="visible"
       exit="exit"
       variants={animationVariants}
     >
-      <S.Wrapper>
-        <S.Title>
-          <p>Empresa não cadastrada</p>
-        </S.Title>
-        <S.ImageContainer
-          src={EspereImg.src}
-          width={400}
-          height={400}
-          priority
-          alt="Empresa não cadastrada"
-        />
+      <S.Title>
+        <p>Empresa não cadastrada</p>
+      </S.Title>
+      <S.ImageContainer
+        src={EspereImg.src}
+        width={400}
+        height={400}
+        priority
+        alt="Empresa não cadastrada"
+      />
 
-        <S.Text>
-          Para anunciar no Excursionistas você precisa cadastrar a sua empresa.
-        </S.Text>
-        <Button onClick={onClick}>Cadastrar Empresa</Button>
-      </S.Wrapper>
-    </motion.div>
+      <S.Text>
+        Para anunciar no Excursionistas você precisa cadastrar a sua empresa.
+      </S.Text>
+      <Button onClick={onClick}>Cadastrar Empresa</Button>
+    </S.Wrapper>
   )
 }
